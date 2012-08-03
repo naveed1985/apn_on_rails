0f207f1e14ccbf7f26b9290c14debe35b218e36e
Naveed Shehzad <naveed.logizest@gmail.com>
2012-08-03 13:10:07 +0500

    trying to fix encoding issue

--- lib/apn_on_rails/app/models/apn/notification.rb
+++ lib/apn_on_rails/app/models/apn/notification.rb
@@ -90,7 +90,7 @@ class APN::Notification < APN::Base
   # Creates the binary message needed to send to Apple.
   def message_for_sending
     json = self.to_apple_json
-    message = "\0\0 #{self.device.to_hexa}\0#{json.length.chr}#{json}"
+    message = "\0\0 #{self.device.to_hexa}\0#{json.length.chr}#{json}".force_encoding('UTF-8')
     raise APN::Errors::ExceededMessageSizeError.new(message) if message.size.to_i > 256
     message
   end
